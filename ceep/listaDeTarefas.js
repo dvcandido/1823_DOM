450ddd54a36b16b93603bb871f1985c0271f9e3f
Douglas Candido <dgxplusplus@gmail.com>
2021-06-10 22:45:59 -0300

    03.Adicionar item na lista

--- ceep/listaDeTarefas.js
+++ ceep/listaDeTarefas.js
@@ -1,8 +1,16 @@
 const criarTarefa = (event) => {
   event.preventDefault();
+  const lista = document.querySelector('[data-list]');
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
-  console.log(valor);
+
+  const tarefa = document.createElement('li');
+  const conteudo = `<p class="content">${valor}</p>`;
+
+  tarefa.classList.add('task');
+  tarefa.innerHTML = conteudo;
+
+  lista.appendChild(tarefa);
   input.value = "";
 };
 
